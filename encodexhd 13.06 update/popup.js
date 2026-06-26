fd79cceeba6961a59703f7b2409bbd5a5de10d83
1337Vaporesso <1337vaporesso@users.noreply.github.com>
2026-06-26 11:39:34 +0300

    fix: hide loader via CSS default display:none instead of JS (avoids DOM race)

--- encodexhd 13.06 update/popup.js
+++ encodexhd 13.06 update/popup.js
@@ -1,6 +1,6 @@
 (function(){
   var L = document.getElementById('patcherLoader');
   var U = document.getElementById('uploadTrigger');
-  if (L) L.hidden = true;
+  if (L) L.remove();
   if (U) U.removeAttribute('hidden');
 })();
